777e8e329fa9a84d8ca67e8a20155e24a1e89386
Joseph Hayfron Acquah <joeacq81@gmail.com>
2024-09-20 16:12:57 +0000

    Print envs

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 import os
 from app import create_app
+from app.config import config
 
 app = create_app(os.environ.get("CONFIG_MODE"), __name__)
+
+print(f"{os.environ.get('CONFIG_MODE')}  {config[os.environ.get('CONFIG_MODE')]}")
 
 
 @app.route("/")
